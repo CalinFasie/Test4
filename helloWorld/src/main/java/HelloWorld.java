7f89eb017396f2903c7cb1bb76feb1b00cfc2d52
Corina Dragan <corina.dragan@endava.com>
2022-02-01 17:47:32 +0200

    New changes

--- helloWorld/src/main/java/HelloWorld.java
+++ helloWorld/src/main/java/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
 
     public static void main(String... args){
         System.out.println("Hello Wolrd! Have you worked with GIT before?");
+        System.out.println("new line added by Corina");
+
     }
 }
