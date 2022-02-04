fc32e99dc4c3668f2ddd91b31adf65c7e134fb2c
CalinFasie <calin79fc@yahoo.com>
2022-02-04 11:45:15 +0200

    Line added locally

--- helloWorld/src/main/java/HelloWorld.java
+++ helloWorld/src/main/java/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("Hello Wolrd! Have you worked with GIT before?");
         System.out.println("new line added by Corina");
         System.out.println("new line added by Calin");
+        System.out.println("line added locally");
 
     }
 }
