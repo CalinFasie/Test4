d8263f559a11051e03b1f9e862e92d37a69d5461
CalinFasie <calin79fc@yahoo.com>
2022-02-04 11:42:02 +0200

    New line in main

--- helloWorld/src/main/java/HelloWorld.java
+++ helloWorld/src/main/java/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String... args){
         System.out.println("Hello Wolrd! Have you worked with GIT before?");
         System.out.println("new line added by Corina");
+        System.out.println("new line added by Calin");
 
     }
 }
