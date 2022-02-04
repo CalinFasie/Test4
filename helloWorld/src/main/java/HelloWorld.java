adfc2004957b1622cd8ec747564ea1004296146b
CalinFasie <calin79fc@yahoo.com>
2022-02-04 15:02:14 +0200

    Delete 1 line

--- helloWorld/src/main/java/HelloWorld.java
+++ helloWorld/src/main/java/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String... args){
         System.out.println("Hello Wolrd! Have you worked with GIT before?");
         System.out.println("new line added by Corina");
-        System.out.println("new line added by Calin");
+
         System.out.println("line added locally");
         System.out.println("new line added on github");
     }
